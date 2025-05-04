0bca3fad2107c4918081404d1f24e0b0dc4863c4
Subendhu-nyk <subendhu.nyk@gmail.com>
2025-05-04 18:41:54 +0530

    added initial control

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,12 @@
+//Defines a SAPUI5 module. sap.ui.define takes an array of dependencies (e.g., ["sap/m/Button"]) and a callback function. This module is loaded via data-sap-ui-onInit in index.html.
 sap.ui.define([
-
-],function (){
+//first we need to pull of our text control and pass it to the function
+"sap/m/Text"
+],function (Text){
     "use strict"
+//second create a new object
+ new Text({
+    text:"Hello Ui5!"
+ }).placeAt('content')
 
-    alert('UI5 ready to go!')
 })
