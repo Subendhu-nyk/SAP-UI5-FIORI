d5631cece14457801dfa67a13b87b46c68dc52d4
Subendhu-nyk <subendhu.nyk@gmail.com>
2025-05-13 16:09:19 +0530

    JS view (rendering index html view  with js)

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,17 @@
-//Defines a SAPUI5 module. sap.ui.define takes an array of dependencies (e.g., ["sap/m/Button"]) and a callback function. This module is loaded via data-sap-ui-onInit in index.html.
-sap.ui.define([
-//first we need to pull of our text control and pass it to the function
-"sap/m/Text"
-],function (Text){
-    "use strict"
-//second create a new object
- new Text({
-    text:"Hello Ui5!"
- }).placeAt('content')
+// //Defines a SAPUI5 module. sap.ui.define takes an array of dependencies (e.g., ["sap/m/Button"]) and a callback function. This module is loaded via data-sap-ui-onInit in index.html.
+// sap.ui.define([
+// //first we need to pull of our text control and pass it to the function
+// "sap/ui/core/mvc/XMLView"
+// ],function (XMLView){
+//     "use strict"
+// //second create a new object or control
+// //  new Text({
+// //     text:"Hello Ui5!"
+// //  }).placeAt('content')
+// XMLView.create({
+//     viewName:"sap.ui.demo.SAP-UI5-FIORI.view.App"
+// }).then(function(oView){
+//     oView.placeAt('content')
+// })
 
-})
+// })
---
+++ webapp/view/Main.view.js
@@ -0,0 +1,31 @@
+sap.ui.jsview("fioriapp.view.Main", {
+
+    /** Specifies the Controller belonging to this View.
+     * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+     * @memberOf fioriapp.view.Main
+     */
+    getControllerName: function() {
+      return "fioriapp.controller.Main";
+    },
+  
+    /** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed.
+     * Since the Controller is given to this method, its event handlers can be attached right away.
+     * @memberOf fioriapp.view.Main
+     */
+    createContent: function(oController) {
+      var oPage = new sap.m.Page({
+        id: "oPage",
+        title: "Title",
+        content: [
+            new sap.m.Text({ text: "This is a JS view." })
+        ]
+      });
+  
+      var app = new sap.m.App("myApp")
+      app.addPage(oPage);
+      app.setInitialPage("oPage");
+      return app;
+    }
+  
+  });
+  
