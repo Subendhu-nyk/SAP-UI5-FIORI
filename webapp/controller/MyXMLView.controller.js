5e195cdf32f411bfcbb86bd51df5388f5aef71d3
Subendhu-nyk <subendhu.nyk@gmail.com>
2025-05-18 12:10:23 +0530

    adding form lgic inside the respective controller

--- webapp/controller/MyXMLView.controller.js
+++ webapp/controller/MyXMLView.controller.js
@@ -3,8 +3,19 @@ sap.ui.define([
 ], function(Controller) {
     "use strict";
     return Controller.extend("fioriapp.controller.MyXMLView", {
-        pressButton: function() {
-            alert("Button pressed");
+        pressButton: function() {            
+            var oUser = this.getView().byId('username')
+            var oPwd =this.getView().byId('idPwd')
+            // oUser and oPwd will return an object not a value
+            // within the controller event handler "this" object is pointing to the controller object.
+            if (oUser.getValue()==="Admin" && oPwd.getValue()=='admin')
+            {
+              alert("Login successfull");  
+            }
+            else{
+                alert("Login Unsuccessfull");  
+            }
+            
         }
     });
 });
